87099a0549018bc32ab45535043ae8b817120c35
LPedroSF <59021127+LPedroSF@users.noreply.github.com>
2024-08-29 19:46:00 +0100

    Update Landing.jsx
    Updated Landing.

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
     <div className='landing'>
       <h1 className='landing__title'>Hello!</h1>
       <h1 className='purple landing__title'>I'm Tristen Hanson!</h1>
-      <p className='landing__subtitle'>I'm an <span className='purple'> artistic graphic designer</span> with </p>
+      <p className='landing__subtitle'>I'm an <span className='purple'> artistic graphic designer</span> with a passion for digital art and creating visual graphics.</p>
       <a target='_blank' href="https://www.instagram.com/swallavo/">
         <FontAwesomeIcon  className="landing__link button" icon="fa-brands fa-instagram" />
       </a>
